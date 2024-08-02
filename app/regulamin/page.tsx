ad436ca443d3e04fd27170c0a26bfd4736b62286
Dawid Komęza <dawid.komeza@gmail.com>
2024-08-02 16:46:58 +0200

    ✨ Style the statute page

--- app/regulamin/page.tsx
+++ app/regulamin/page.tsx
@@ -4,6 +4,8 @@ import { getStatute } from "@/app/lib/oci";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
+import "./style.scss";
+
 export default async function Page() {
   const statute = await getStatute();
 
@@ -14,20 +16,16 @@ export default async function Page() {
       </Link>
       <Navbar />
       <div className="container mx-auto pb-8">
-        <main className="pt-4 md:pt-6 lg:pt-8 xl:pt-12 mt-8 sm:mt-4 bg-dimmedBlue pb-8 rounded-t-2xl">
+        <main className="pt-4 md:pt-6 lg:pt-8 xl:pt-12 mt-8 sm:mt-4 bg-[#61769A] pb-8 rounded-t-2xl">
           <h2 className="text-white text-3xl sm:text-4xl xl:text-5xl uppercase text-center">
             regulamin
           </h2>
           <div
-            className="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl mt-4"
+            className="regulamin"
             dangerouslySetInnerHTML={{ __html: statute }}
           />
         </main>
-        <img
-          src="/images/bg-contact-bottom.svg"
-          alt=""
-          className="w-full -mt-px"
-        />
+        <img src="/images/bg-statue.svg" alt="" className="w-full -mt-px" />
       </div>
     </>
   );
