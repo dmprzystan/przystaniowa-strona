aa3a8424075f05b22dfc8a2b64785e84e5cad0ef
Dawid Komęza <dawid.komeza@gmail.com>
2024-08-05 15:32:27 +0200

    ✨ Add more logs to prebuild

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,5 +1,5 @@
 const { config } = require("dotenv")
-const { writeFileSync, mkdirSync } = require("fs")
+const { writeFileSync, mkdirSync, statSync } = require("fs")
 const { join } = require("path")
 
 config({path: `.env`})
@@ -16,6 +16,8 @@ const key_dir = join(__dirname, "..", oci_key_file.split("/").slice(0, -1).join(
 
 mkdirSync(key_dir, {recursive: true})
 writeFileSync(key_path, oci_key, {encoding: "utf-8"})
+const keyFile = statSync(key_path)
+
 
 const oci_config = {
     user: process.env.OCI_USER,
@@ -37,4 +39,5 @@ for (const [key, value] of Object.entries(oci_config)) {
     writeFileSync(config_path, `${key}=${value}\n`, {flag: "a"})
 }
 
+console.log(`Wrote OCI key to ${key_path} (${keyFile.size} bytes)`)
 console.log(`Wrote OCI config to ${config_path}`)
