4c05acd89d1dbdfb9e2151196321a4317277f777
Dawid Komęza <dawid.komeza@gmail.com>
2024-10-21 11:42:55 +0200

    💄 Finish the confirmation page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,80 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "o-nas": "url('/images/bg-o-nas.svg')",
-        plan: "url('/images/bg-plan.svg')",
-      },
-      backgroundSize: {
-        full: "101% 100%",
-      },
-      colors: {
-        dimmedBlue: {
-          DEFAULT: "#4F5D75",
-        },
-      },
-      boxShadow: {
-        arround: "0px 2px 8px 0px rgba(0, 0, 0, 0.25);",
-        top: "0px -4px 6px 0px rgba(0, 0, 0, 0.125);",
-        bottom: "0px 4px 6px 0px rgba(0, 0, 0, 0.125);",
-        bottomShadow: "inset 0px -35px 25px -35px #F2F2F2;",
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+			"o-nas": "url('/images/bg-o-nas.svg')",
+			plan: "url('/images/bg-plan.svg')",
+  		},
+  		backgroundSize: {
+  			full: '101% 100%'
+  		},
+  		colors: {
+  			dimmedBlue: {
+  				DEFAULT: '#4F5D75'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		boxShadow: {
+  			arround: '0px 2px 8px 0px rgba(0, 0, 0, 0.25);',
+  			top: '0px -4px 6px 0px rgba(0, 0, 0, 0.125);',
+  			bottom: '0px 4px 6px 0px rgba(0, 0, 0, 0.125);',
+  			bottomShadow: 'inset 0px -35px 25px -35px #F2F2F2;'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
+  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar"), require("tailwindcss-animate")],
   variants: {
     extend: {
       display: ["last"],
