2674f36c5153c1db733b1445fa56efcc8435047d
Dawid Komęza <dawid.komeza@gmail.com>
2024-10-22 22:41:00 +0200

    🔨 Refactor the schedule page

--- app/admin/plan/components/Schedule.tsx
+++ app/admin/plan/components/Schedule.tsx
@@ -1,107 +1,106 @@
 "use client";
 
 import { useState } from "react";
-import ScheduleItem from "./ScheduleItem";
-import { useMessage } from "../../AdminContext";
+import { DeleteOutlineRounded } from "@mui/icons-material";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+
+type Schedule = Record<
+  string,
+  { title: string; time: string; id: string; day: string }[]
+>;
 
 type ScheduleProps = {
-  scheduledDays: Record<
-    string,
-    { title: string; time: string; id: string; day: string }[]
-  >;
+  scheduledDays: Schedule;
 };
 
 const APILink = "/api/admin/schedule";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+import { toast } from "sonner";
+
+const newEventSchema = z.object({
+  title: z.string().min(1, { message: "Tytuł nie może być pusty" }),
+  time: z.string().min(1, { message: "Godzina nie może być pusta" }),
+});
+
+const days = [
+  "poniedziałek",
+  "wtorek",
+  "środa",
+  "czwartek",
+  "piątek",
+  "sobota",
+  "niedziela",
+];
+
 function Schedule(props: ScheduleProps) {
-  const { addToast } = useMessage();
   const [clientSchedule, setClientSchedule] = useState(props.scheduledDays);
-
-  const days = [
-    "poniedziałek",
-    "wtorek",
-    "środa",
-    "czwartek",
-    "piątek",
-    "sobota",
-    "niedziela",
-  ];
-
-  const handleRemove = async (id: string) => {
-    setClientSchedule((prev) => {
-      const newSchedule = { ...prev };
-      for (const day in newSchedule) {
-        newSchedule[day] = newSchedule[day].filter((item) => item.id !== id);
-      }
-      return newSchedule;
-    });
-
-    const res = await fetch(`${APILink}/${id}`, {
-      method: "DELETE",
-    });
-
-    if (res.ok) {
-      addToast({ type: "success", message: "Pomyśnie usunięto wydarzenie" });
-    } else {
-      try {
-        const data = await res.json();
-        if (data.error) addToast({ type: "error", message: data.error });
-        else addToast({ type: "error", message: "Wystąpił nieznany błąd" });
-      } catch {
-        addToast({ type: "error", message: "Wystąpił nieznany błąd" });
-      }
-    }
-
-    getNewSchedule();
-  };
 
   const handleAdd = async (
     e: React.FormEvent<HTMLFormElement>,
     day: string
   ) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const data = new FormData(form);
-
-    const title = data.get("title") as string;
-    const time = data.get("time") as string;
-
-    if (!title || !time) {
-      return;
-    }
-
-    setClientSchedule((prev) => {
-      const newSchedule = { ...prev };
-      if (!newSchedule[day]) {
-        newSchedule[day] = [];
-      }
-      newSchedule[day] = [
-        ...newSchedule[day],
-        { title, time, id: Math.random().toString(), day },
-      ].sort((a, b) => a.time.localeCompare(b.time));
-      return newSchedule;
-    });
-
-    const res = await fetch(APILink, {
-      method: "POST",
-      body: JSON.stringify({ title, time, day }),
-    });
-
-    if (res.ok) {
-      addToast({ type: "success", message: "Pomyśnie dodano wydarzenie" });
-    } else {
-      try {
-        const data = await res.json();
-        if (data.error) addToast({ type: "error", message: data.error });
-        else addToast({ type: "error", message: "Wystąpił nieznany błąd" });
-      } catch {
-        addToast({ type: "error", message: "Wystąpił nieznany błąd" });
-      }
-    }
-
-    form.reset();
-
-    getNewSchedule();
+    // e.preventDefault();
+    // const form = e.currentTarget;
+    // const data = new FormData(form);
+    // const title = data.get("title") as string;
+    // const time = data.get("time") as string;
+    // if (!title || !time) {
+    //   return;
+    // }
+    // setClientSchedule((prev) => {
+    //   const newSchedule = { ...prev };
+    //   if (!newSchedule[day]) {
+    //     newSchedule[day] = [];
+    //   }
+    //   newSchedule[day] = [
+    //     ...newSchedule[day],
+    //     { title, time, id: Math.random().toString(), day },
+    //   ].sort((a, b) => a.time.localeCompare(b.time));
+    //   return newSchedule;
+    // });
+    // const res = await fetch(APILink, {
+    //   method: "POST",
+    //   body: JSON.stringify({ title, time, day }),
+    // });
+    // if (res.ok) {
+    //   addToast({ type: "success", message: "Pomyśnie dodano wydarzenie" });
+    // } else {
+    //   try {
+    //     const data = await res.json();
+    //     if (data.error) addToast({ type: "error", message: data.error });
+    //     else addToast({ type: "error", message: "Wystąpił nieznany błąd" });
+    //   } catch {
+    //     addToast({ type: "error", message: "Wystąpił nieznany błąd" });
+    //   }
+    // }
+    // form.reset();
+    // getNewSchedule();
   };
 
   const getNewSchedule = async () => {
@@ -124,73 +123,220 @@ function Schedule(props: ScheduleProps) {
   };
 
   return (
-    <div className="px-4 md:px-6 xl:px-8 2xl:px-16 rounded-3xl mt-4 md:mt-0">
-      <h2 className="text-4xl text-left">Plan tygodnia</h2>
-      <div className="flex flex-wrap mt-8 gap-8 justify-around">
+    <div className="px-4 w-full mt-4">
+      <h2 className="text-4xl text-center">Plan tygodnia</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 mt-8 gap-8 justify-around pb-6">
         {days &&
           days.map((day) => (
-            <div
+            <ScheduleDay
               key={day}
-              className="flex-grow w-full sm:w-[48%] relative border border-black rounded-3xl py-3 pt-6 px-4 last-of-type:flex-grow-0 flex flex-col justify-between"
-            >
-              <h3 className="text-lg absolute bg-[#F2F2F2] -top-4 px-2 left-6">
-                {day}
-              </h3>
-              <ul className="text-lg pb-4 flex-col flex gap-4 cursor-pointer">
-                {clientSchedule[day] &&
-                  clientSchedule[day].map(({ title, time, id }) => (
-                    <ScheduleItem
-                      key={id}
-                      title={title}
-                      time={time}
-                      id={id}
-                      handleRemove={handleRemove}
-                    />
-                  ))}
-              </ul>
-              <form
-                onSubmit={(e) => handleAdd(e, day)}
-                className="flex flex-col sm:flex-row gap-4 md:gap-2 lg:gap-4 shadow-arround px-4 py-3 rounded-2xl items-center"
-              >
-                <input
-                  type="time"
-                  name="time"
-                  id=""
-                  className="hidden sm:block px-2 2xl:px-4 py-2 rounded-lg shadow-arround flex-shrink-0 focus:outline-none focus:bg-gray-100 transition duration-200  w-full sm:w-auto"
-                  defaultValue={"12:00"}
-                />
-                <input
-                  type="text"
-                  name="title"
-                  className="w-full shadow-arround focus:outline-none focus:bg-gray-100 transition duration-200 px-2 2xl:px-4 py-2 rounded-lg"
-                  placeholder="Tytuł"
-                />
-                <button
-                  type="submit"
-                  className="hidden sm:block text-white bg-green-500 shadow-arround px-2 2xl:px-4 py-2 rounded-xl hover:bg-green-600 transition duration-200 flex-shrink-0 w-full sm:w-auto"
-                >
-                  Dodaj
-                </button>
-                <div className="sm:hidden grid grid-cols-2 w-full gap-2">
-                  <input
-                    type="time"
-                    name="time"
-                    id=""
-                    className="px-4 py-2 rounded-lg shadow-arround focus:outline-none focus:bg-gray-100 transition duration-200 w-full"
-                    defaultValue={"12:00"}
-                  />
-                  <button
-                    type="submit"
-                    className="bg-white shadow-arround px-4 py-2 rounded-xl hover:bg-gray-100 transition duration-200 w-full"
-                  >
-                    Dodaj
-                  </button>
-                </div>
-              </form>
-            </div>
+              day={day}
+              schedule={clientSchedule}
+              getNewSchedule={getNewSchedule}
+              setClientSchedule={setClientSchedule}
+            />
           ))}
       </div>
     </div>
+  );
+}
+
+function ScheduleDay({
+  day,
+  schedule,
+  getNewSchedule,
+  setClientSchedule,
+}: {
+  day: string;
+  schedule: Schedule;
+  getNewSchedule: () => void;
+  setClientSchedule: React.Dispatch<React.SetStateAction<Schedule>>;
+}) {
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof newEventSchema>>({
+    resolver: zodResolver(newEventSchema),
+    defaultValues: {
+      title: "",
+      time: "12:00",
+    },
+  });
+
+  const getDate = (day: string) => {
+    const today = dayjs();
+    let dayIndex = days.indexOf(day);
+    // Convert to 0-6 index where 0 is Sunday and 6 is Saturday
+    if (dayIndex === 6) dayIndex = 0;
+    else dayIndex += 1;
+
+    const todayIndex = today.day();
+    const diff = dayIndex - todayIndex;
+
+    return today.add(diff, "day");
+  };
+
+  const add = async (data: z.infer<typeof newEventSchema>) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(APILink, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...data, day }),
+      });
+
+      if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.error);
+      }
+
+      toast.success("Pomyśnie dodano wydarzenie");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      getNewSchedule();
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Card className="w-full">
+      <CardHeader className="py-4">
+        <CardTitle className="capitalize flex items-center gap-2">
+          {day}
+          <span className="text-sm font-normal">
+            {getDate(day).locale("pl").format("DD MMMM YYYY")}
+          </span>
+        </CardTitle>
+      </CardHeader>
+      <Separator />
+      <CardContent className="mt-6 flex flex-col gap-3">
+        {schedule[day] &&
+          schedule[day].map(({ title, time, id }) => (
+            <ScheduleItem
+              key={id}
+              title={title}
+              time={time}
+              id={id}
+              getNewSchedule={getNewSchedule}
+            />
+          ))}
+      </CardContent>
+      <Separator />
+      <CardFooter className="mt-2">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(add)}>
+            <div className="flex items-center gap-2">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        className="text-base !mt-0.5"
+                        placeholder="Tytuł"
+                        required
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.title?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="time"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="time"
+                        step="900"
+                        placeholder="Tytuł"
+                        required
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.time?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button
+              type="submit"
+              size="sm"
+              className="!mt-2 w-full"
+              disabled={loading}
+            >
+              {loading ? (
+                <div className="border-2 rounded-full border-s-transparent h-4 w-4 animate-spin" />
+              ) : (
+                <PlusCircledIcon className="w-4 h-4" />
+              )}
+              Dodaj
+            </Button>
+          </form>
+        </Form>
+      </CardFooter>
+    </Card>
+  );
+}
+
+type ScheduleItemProps = {
+  title: string;
+  time: string;
+  id: string;
+  getNewSchedule: () => void;
+};
+
+function ScheduleItem(props: ScheduleItemProps) {
+  const [loading, setLoading] = useState(false);
+  const { title, time, id, getNewSchedule } = props;
+
+  const handleRemove = async (id: string) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${APILink}/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.error);
+      }
+
+      toast.success("Pomyśnie usunięto wydarzenie");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      getNewSchedule();
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Card>
+      <CardContent className="flex justify-between !p-3">
+        <div className="flex flex-col">
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{time}</CardDescription>
+        </div>
+        <Button size="icon" disabled={loading} onClick={() => handleRemove(id)}>
+          {loading ? (
+            <div className="border-2 rounded-full border-s-transparent h-4 w-4 animate-spin" />
+          ) : (
+            <DeleteOutlineRounded />
+          )}
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
 
