4343789477970ce479725b301dde7e9fe60746eb
Dawid Komęza <dawid.komeza@gmail.com>
2024-12-31 15:24:33 +0100

    🚚 Move email tile to separate folder

--- app/admin/components/StronaGlowna.tsx
+++ app/admin/components/StronaGlowna.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import EmailTile from "./EmailTile";
+import EmailTile from "./tiles/EmailTile";
 
 function StronaGlowna() {
   return (
