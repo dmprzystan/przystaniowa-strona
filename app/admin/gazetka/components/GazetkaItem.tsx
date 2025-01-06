75493167369f6827034488877be4a3640ebae87c
Dawid Komęza <dawid.komeza@gmail.com>
2025-01-06 18:08:57 +0100

    🔥 Temporarily remove newspaper editing

--- app/admin/gazetka/components/GazetkaItem.tsx
+++ app/admin/gazetka/components/GazetkaItem.tsx
@@ -7,6 +7,7 @@ import dayjs from "dayjs";
 import "dayjs/locale/pl";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Cross2Icon, BookmarkIcon } from "@radix-ui/react-icons";
 dayjs.locale("pl");
 
 type GazetkaItemProps = {
@@ -47,56 +48,7 @@ function GazetkaItem(props: GazetkaItemProps) {
   return (
     <Card key={newspaper.id} className="p-4">
       {editMode ? (
-        <form
-          className="flex flex-col sm:flex-row items-start sm:items-center gap-4 justify-between"
-          onSubmit={(e) => {
-            handleEdit(e, newspaper.id);
-            setEditMode(false);
-          }}
-        >
-          <div className="flex flex-col lg:flex-row items-start sm:items-center gap-4 w-full xl:w-auto">
-            <div className="flex flex-row items-start sm:items-end gap-4 w-full xl:w-auto">
-              <div className="flex flex-row items-end gap-1">
-                <p className="font-light pl-2 sm:pl-0">nr.</p>
-                <input
-                  className="text-xl px-2 py-1 rounded-lg shadow-arround focus:outline-none focus:bg-gray-100 transition duration-200 w-16"
-                  type="number"
-                  name="title"
-                  id="title"
-                  defaultValue={newspaper.title}
-                />
-              </div>
-              <input
-                type="date"
-                className="text-xl px-2 py-1 rounded-lg shadow-arround focus:outline-none focus:bg-gray-100 transition duration-200 w-full sm:w-40 xl:w-44 bg-white"
-                defaultValue={newspaper.date.toISOString().split("T")[0]}
-                name="date"
-                id="date"
-              />
-            </div>
-            <input
-              className="w-full shadow-arround focus:outline-none focus:bg-gray-100 transition duration-200 rounded-lg file:bg-gray-100 file:outline-none file:border-none file:px-4 file:py-2 file:mr-2 pr-2"
-              id="file-upload"
-              type="file"
-              name="file"
-              accept=".pdf"
-            />
-          </div>
-          <div className="flex items-center md:flex-col lg:flex-row gap-4 sm:gap-2 md:gap-4 justify-between lg:justify-normal w-full lg:w-auto">
-            <button
-              className="bg-red-500 text-white rounded-lg py-2 px-4 shadow-none hover:shadow-lg duration-300 transition-all"
-              onClick={() => setEditMode(false)}
-            >
-              Anuluj
-            </button>
-            <button
-              type="submit"
-              className="bg-green-500 text-white rounded-lg py-2 px-4 shadow-none hover:shadow-lg duration-300 transition-all"
-            >
-              Zapisz
-            </button>
-          </div>
-        </form>
+        <></>
       ) : (
         <div className="flex items-center gap-4 justify-between">
           <div className="flex items-center gap-2 md:gap-4">
@@ -123,7 +75,7 @@ function GazetkaItem(props: GazetkaItemProps) {
               size="icon"
               variant="outline"
               onClick={() => setEditMode(true)}
-              disabled={loading}
+              disabled
             >
               <EditRounded />
             </Button>
