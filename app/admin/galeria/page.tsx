b384b12955da41a313f5960ca330476b932c1135
Dawid Komęza <dawid.komeza@gmail.com>
2025-02-13 13:13:00 +0100

    ✨ Create skeleton loading for the gallery page

--- app/admin/galeria/page.tsx
+++ app/admin/galeria/page.tsx
@@ -16,6 +16,7 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 import dayjs from "dayjs";
 import "dayjs/locale/pl";
+import { Skeleton } from "@/components/ui/skeleton";
 
 dayjs.locale("pl");
 
@@ -63,46 +64,97 @@ export default function Page() {
           <NewAlbum refresh={fetchGallery} />
         </div>
         <div className="gallery grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-8 md:mt-16">
-          {gallery.map((album) => (
-            <Link
-              key={album.id}
-              className="col-span-1"
-              href={`/admin/galeria/${album.id}`}
-            >
-              <Card className="h-full w-full relative overflow-hidden hover:shadow-arround transition-all duration-200">
-                <AspectRatio ratio={4 / 3}>
-                  {album.thumbnail || album.photos.length > 0 ? (
-                    <img
-                      src={`/public/${
-                        album.thumbnail
-                          ? album.thumbnail.url
-                          : album.photos[0].url
-                      }`}
-                      alt=""
-                      className="block object-cover h-full w-full"
-                    />
-                  ) : (
-                    <div className="bg-gray-200 w-full h-full flex items-center justify-center flex-col gap-2">
-                      <ImageNotSupportedRounded className="text-gray-500" />
-                      <p className="text-lg text-gray-500">Brak zdjęć</p>
-                    </div>
-                  )}
-                </AspectRatio>
-                <div className="absolute bottom-0 left-0 right-0 px-4 py-3 bg-white bg-opacity-40 backdrop-blur-md flex items-center gap-2">
-                  <h3 className="text-xl font-bold text-center">
-                    {album.title}
-                  </h3>
-                  <div className="w-1 h-1 bg-muted-foreground rounded-full" />
-                  <p className="text-sm font-light">
-                    {dayjs(album.date).format("MMMM YYYY")}
-                  </p>
-                </div>
-              </Card>
-            </Link>
-          ))}
+          {loading ? (
+            <>
+              {Array.from({ length: cachedNumber }).map((_, i) => (
+                <SkeletonAlbum key={i} />
+              ))}
+            </>
+          ) : (
+            <>
+              {gallery.map((album) => (
+                <AlbumThumbnail album={album} key={album.id} />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </>
+  );
+}
+
+function SkeletonAlbum() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 8) + 8;
+
+    const randomString = Array.from({ length: random })
+      .map(() => String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+      .join("");
+
+    setTitle(randomString);
+
+    const randomMonth = Math.floor(Math.random() * 12) + 1;
+    const randomYear = Math.floor(Math.random() * 15) + 2010;
+
+    const randomDate = new Date(randomYear, randomMonth, 1);
+
+    setDate(dayjs(randomDate).format("MMMM YYYY"));
+  }, []);
+
+  return (
+    <Card className="h-full w-full relative overflow-hidden">
+      <AspectRatio ratio={4 / 3}>
+        <Skeleton className="w-full h-full" />
+      </AspectRatio>
+      <div className="absolute bottom-0 left-0 right-0 px-4 py-3 bg-white bg-opacity-40 backdrop-blur-md flex items-center gap-2">
+        <Skeleton className="text-xl font-bold text-center px-2 text-transparent">
+          {title}
+        </Skeleton>
+        <div className="w-1 h-1 bg-muted-foreground rounded-full" />
+        <Skeleton className="text-sm font-light text-transparent">
+          {date}
+        </Skeleton>
+      </div>
+    </Card>
+  );
+}
+
+function AlbumThumbnail({ album }: { album: Album }) {
+  return (
+    <Link
+      key={album.id}
+      className="col-span-1"
+      href={`/admin/galeria/${album.id}`}
+    >
+      <Card className="h-full w-full relative overflow-hidden hover:shadow-arround transition-all duration-200">
+        <AspectRatio ratio={4 / 3}>
+          {album.thumbnail || album.photos.length > 0 ? (
+            <img
+              src={`/public/${
+                album.thumbnail ? album.thumbnail.url : album.photos[0].url
+              }`}
+              alt=""
+              className="block object-cover h-full w-full"
+            />
+          ) : (
+            <div className="bg-gray-200 w-full h-full flex items-center justify-center flex-col gap-2">
+              <ImageNotSupportedRounded className="text-gray-500" />
+              <p className="text-lg text-gray-500">Brak zdjęć</p>
+            </div>
+          )}
+        </AspectRatio>
+        <div className="absolute bottom-0 left-0 right-0 px-4 py-3 bg-white bg-opacity-40 backdrop-blur-md flex items-center gap-2">
+          <h3 className="text-xl font-bold text-center">{album.title}</h3>
+          <div className="w-1 h-1 bg-muted-foreground rounded-full" />
+          <p className="text-sm font-light">
+            {dayjs(album.date).format("MMMM YYYY")}
+          </p>
+        </div>
+      </Card>
+    </Link>
   );
 }
 
